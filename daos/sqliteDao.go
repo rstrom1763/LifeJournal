9c3c89920e43a717b523b5dccc3445b7fc881e96
Ryan <48137622+rstrom1763@users.noreply.github.com>
2026-02-22 01:13:34 -0800

    Moved sqlite init to sqlite dao

--- daos/sqliteDao.go
+++ daos/sqliteDao.go
@@ -6,11 +6,152 @@ import (
 	"log"
 
 	. "memories/model"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 // SQLiteDAO implementation
 type SQLiteDAO struct {
 	db *sql.DB
+}
+
+// Create the SQLite DB connection and create the tables if they don't already exist
+func InitSQLiteDB(path string) *sql.DB {
+	createTablesQuery := `CREATE TABLE IF NOT EXISTS video_games (
+    id INT,
+    title VARCHAR(255),
+    notes TEXT,
+    multiplayer BOOLEAN,
+    PRIMARY KEY (id)
+);
+CREATE TABLE IF NOT EXISTS users (
+    uuid CHAR(36),
+    email VARCHAR(255),
+    password_hash VARCHAR(255),
+    salt VARCHAR(255),
+    created TIMESTAMP,
+    PRIMARY KEY (uuid)
+);
+CREATE TABLE IF NOT EXISTS theater_movies (
+    id INT,
+    title VARCHAR(255),
+    date DATE,
+    people_went_with TEXT,
+    notes TEXT,
+    PRIMARY KEY (id)
+);
+CREATE TABLE IF NOT EXISTS watched_movies (
+    title VARCHAR(255),
+    rating VARCHAR(50),
+    tier VARCHAR(50),
+    notes TEXT,
+    PRIMARY KEY (title)
+);
+CREATE TABLE IF NOT EXISTS travel (
+    title VARCHAR(255),
+    places TEXT,
+    people_went_with TEXT,
+    notes TEXT,
+    dates DATE,
+    id INT,
+    PRIMARY KEY (id)
+);
+CREATE TABLE IF NOT EXISTS tv_shows (
+    title VARCHAR(255),
+    date DATE,
+    notes TEXT,
+    seasons_watched TEXT,
+    childhood_show BOOLEAN,
+    PRIMARY KEY (title)
+);
+CREATE TABLE IF NOT EXISTS books (
+    title VARCHAR(255),
+    date_finished DATE,
+    author VARCHAR(255),
+    rating FLOAT,
+    series VARCHAR(255),
+    owned BOOLEAN,
+    pages INT,
+    series_sequence INT,
+    finished BOOLEAN,
+    PRIMARY KEY (title)
+);
+CREATE TABLE IF NOT EXISTS food_places (
+    name VARCHAR(255),
+    type VARCHAR(255),
+    location VARCHAR(255),
+    notes TEXT,
+    category VARCHAR(50),
+    PRIMARY KEY (name)
+);
+CREATE TABLE IF NOT EXISTS life_events (
+    id INT,
+    title VARCHAR(255),
+    month INT,
+    day INT,
+    year INT,
+    notes TEXT,
+    PRIMARY KEY (id)
+);
+CREATE TABLE IF NOT EXISTS concerts (
+    date DATE,
+    artists TEXT,
+    notes TEXT,
+    people_went_with TEXT,
+    PRIMARY KEY (date)
+);
+CREATE TABLE IF NOT EXISTS people (
+    id INT,
+    first VARCHAR(255),
+    middle VARCHAR(255),
+    last VARCHAR(255),
+    address VARCHAR(255),
+    birth_day INT,
+    birth_month INT,
+    birth_year INT,
+    gift_ideas TEXT[],
+    email VARCHAR(255),
+    category VARCHAR(50),
+    notes TEXT,
+    PRIMARY KEY (id)
+);
+CREATE TABLE IF NOT EXISTS random_memories (
+    id INT,
+    date DATE,
+    notes TEXT,
+    involved_people TEXT,
+    PRIMARY KEY (id)
+);
+CREATE TABLE IF NOT EXISTS journal_entries (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created DATETIME DEFAULT CURRENT_TIMESTAMP,
+    entry TEXT NOT NULL,
+    title VARCHAR(255),
+    tags TEXT,
+    photos TEXT
+);
+CREATE TABLE IF NOT EXISTS files (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	bytes BLOB NOT NULL,
+	file_name VARCHAR(255) NOT NULL,
+	created DATETIME DEFAULT CURRENT_TIMESTAMP
+);`
+
+	db, err := sql.Open("sqlite3", path)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Could not open DB: %s", err))
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Could not ping DB: %s", err))
+	}
+
+	_, err = db.Exec(createTablesQuery)
+	if err != nil {
+		log.Fatalf("Could not create tables: %s", err)
+	}
+
+	return db
 }
 
 // NewSQLiteDAO creates a new SQLite DAO
